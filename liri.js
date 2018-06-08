4e2975474e3fb7f7c736fe0dbdacbc8e878813c8
yeungpirate <yeungpirate@gmail.com>
2018-06-08 05:33:25 -0700

    basic functions done, except for spotify

--- liri.js
+++ liri.js
@@ -4,9 +4,13 @@
 
 require('dotenv').config();
 var movieThis = require('./omdb');
+var fs = require('fs');
 var spotifyThisSong = require('./spotifyThis');
 var myTweets = require('./myTweets');
 var keys = require('./keys.js');
+var spotify = require('node-spotify-api');
+var spotify = new spotify(keys.spotify);
+var request = require('request');
 
 // ===============
 // || VARIABLES ||
@@ -17,6 +21,51 @@ var action = process.argv[2];
 // ===============
 // || FUNCTIONS ||
 // ===============
+
+function doWhatItSays() {
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (!err) {
+            var data = data.split(',');
+            var action = data[0];
+            var userQuery = data[1];
+            if (action === 'my-tweets') {
+                myTweets();
+            }
+            else if (action === 'spotify-this-song') {
+                if (!userQuery) {
+                    var userQuery = 'the+sign';
+                }
+                var params = {
+                    type: 'track',
+                    query: userQuery.trim()
+                };
+                spotify.search(params, function(err, data) {
+                    if (!err) {
+                        console.log(data);
+                    }
+                });
+            }
+            else if (action === 'movie-this') {
+                if (!userQuery) {
+                    var userQuery = 'mr+nobody';
+                }
+                var omdbKey = process.env.apikey;
+                var queryURL = `http://www.omdbapi.com/?apikey=${omdbKey}&t=${userQuery.trim()}`
+                request(queryURL, function(err, data, response) {
+                    var jsonData = JSON.parse(response);
+                    console.log('Title: ' + jsonData.Title + 
+                    '\nReleased Year: ' + jsonData.Year + 
+                    '\nIMDB Rating: ' + jsonData.Ratings[0].Value + 
+                    '\nRotten Tomatoes Rating: ' + jsonData.Ratings[1].Value + 
+                    '\nProduced In: ' + jsonData.Country + 
+                    '\nLanguage: ' + jsonData.Language + 
+                    '\nPlot: ' + jsonData.Plot + 
+                    '\nActors: ' + jsonData.Actors);
+                });
+            }
+        }
+    });
+};
 
 // =================
 // || APPLICATOIN ||
--- spotifyThis.js
+++ spotifyThis.js
@@ -2,7 +2,7 @@ var spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 var spotify = new spotify(keys.spotify);
 
-function spotifyThisSong() {
+function spotifyThisSong(song) {
     if (process.argv.length > 3) {
         var song = process.argv.slice(3).join('+');
     }
