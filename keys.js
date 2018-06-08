5bcfb24080e5ea17e8c7d9412d551165c247abeb
yeungpirate <yeungpirate@gmail.com>
2018-06-07 19:21:45 -0700

    got keys working, step 9 complete

--- keys.js
+++ keys.js
@@ -3,8 +3,8 @@ console.log('this is loaded');
 exports.twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    acess_token_key: process.env.TWITTER_ACESS_TOKEN_KEY,
-    acess_token_secret: process.env.TWITTER_ACESS_TOKEN_SECRET
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,6 +1,64 @@
-require('dotenv').config();
-var keys = require('./keys.js');
+// ==================
+// || REQUIREMENTS ||
+// ==================
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+require('dotenv').config();
+
+// ===============
+// || VARIABLES ||
+// ===============
+
+var keys = require('./keys.js');
+var action = process.argv[3];
+
+// ===============
+// || FUNCTIONS ||
+// ===============
+
+function twitter(keys) {
+    this.keys = keys;
+};
+
+function spotify(keys) {
+    this.keys = keys;
+};
+
+// function myTweets() {
+    
+// };
+
+// function spotifyThisSong() {
+
+// };
+
+// function movieThis() {
+
+// };
+
+// function doWhatItSays() {
+
+// };
+
+// =================
+// || APPLICATOIN ||
+// =================
+
+// if (action === 'my-tweets') {
+//     myTweets();
+// }
+// else if (action === 'spotify-this-song') {
+//     spotifyThisSong();
+// }
+// else if (action === 'movie-this') {
+//     movieThis();
+// }
+// else if (action === 'do-what-it-says') {
+//     doWhatItSays();
+// }
+
+
+var spotify = new spotify(keys.spotify);
+var client = new twitter(keys.twitter);
+console.log(spotify);
+console.log(client);
 
